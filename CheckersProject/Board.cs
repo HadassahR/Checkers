4f27f57d8a9e2a04fb72935fab7877636afbbe3f
HadassahR <58051299+HadassahR@users.noreply.github.com>
2021-12-15 22:12:47 -0500

    Initial commit

---
+++ CheckersProject/Board.cs
@@ -0,0 +1,79 @@
+﻿using CheckersProject;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace CheckersGame
+{
+    public class Board // Leah // 
+    {
+        Button[,] board;
+        private Player startingPlayer;
+        private Player currentPlayer;
+        public static readonly int SIZE = 8;
+
+
+        public Board(Button[,] board)
+        {
+            this.board = board;
+        }
+
+        /* Computer is MAX, User is MIN. This method should only be used once */
+        public void setStartingPlayer(Player player)
+        {
+            startingPlayer = player;
+            currentPlayer = startingPlayer;
+        }
+
+        public Player getStartingPlayer()
+        {
+            return startingPlayer;
+        }
+
+        public void nextPlayersTurn()
+        {
+            currentPlayer = currentPlayer == Player.MAX ? Player.MIN : Player.MAX;
+        }
+
+        public Player getCurrentPlayer()
+        {
+            return currentPlayer;
+        }
+        /*
+         * calculates and returns a heuristic value for the current board
+         */
+        internal double heuristicValue()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /*
+         * returns a boolean that is true if the specified player can move on that piece
+         * note: must check that there is a piece in that square
+         */
+        internal bool canMove(Player player, int col, int row)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /*
+         * returns a list of boards for every possible next move
+         * will call canMove
+         */
+        internal Board[] allMoves(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /*
+         * returns a list of possible moves for the piece specified in that column
+         */
+        internal List<Board> MakeMove(Player player, int col, int row)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CheckersProject/CheckersForm.cs
@@ -0,0 +1,107 @@
+﻿using CheckersGame;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CheckersProject
+{
+    public partial class CheckersForm : Form
+    {
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CheckersForm));
+        Button[,] button;
+        Board gameBoard;
+        const int BOARD_SIZE = 8;
+        public CheckersForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckerForm_Load(object sender, EventArgs e)
+        {
+            button = new Button[8, 8] {
+                { square1 , square2, square3, square4, square5, square6, square7, square8       },
+                { square9, square10, square11, square12, square13, square14, square15, square16 },
+                { square17, square18, square19, square20, square21, square22, square23, square24},
+                { square25, square26, square27, square28, square29, square30, square31, square32},
+                { square33, square34, square35, square36, square37, square38, square39, square40},
+                { square41, square42, square43, square44, square45, square46, square47, square48},
+                { square49, square50, square51, square52, square53, square54, square55, square56},
+                { square57, square58, square59, square60, square61, square62, square63, square64}
+            };
+            gameBoard = new Board(button);
+        }
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            InitializeNoneCheckers();
+            InitializeBoard();
+            startingPlayer.Visible = false;
+            level.Visible = false;
+            var startBtn = (Button)sender;
+            startBtn.Visible = false;
+        }
+        private void InitializeNoneCheckers()
+        {
+            foreach (var btn in button)
+            {
+                btn.BackgroundImage = Properties.Resources.checkerNone;
+                btn.Tag = "none";
+                btn.BackgroundImageLayout = ImageLayout.Stretch; 
+                if (btn.BackColor == Color.Black)
+                {
+                    btn.Enabled = false; 
+                }
+                //btn.Text = "none!"; 
+            }
+        }
+        private void InitializeBoard()
+        {
+            if (radioYou.Checked)
+            {
+                InitializeTop(Properties.Resources.checkerGray, "white");
+                InitializeBottom(Properties.Resources.checkerWhite, "gray");
+                gameBoard.setStartingPlayer(Player.MIN);
+            }
+            else if (radioComputer.Checked)
+            {
+                InitializeTop(Properties.Resources.checkerWhite, "gray");
+                InitializeBottom(Properties.Resources.checkerGray, "white");
+                gameBoard.setStartingPlayer(Player.MAX);
+            }
+        }
+        private void InitializeTop(Bitmap checker, string color)
+        {
+            int lastColumn = 2;
+            int boardSize = 8;
+            for (int col = 0; col <= lastColumn; col++)
+            {
+                for (int row = 0; row < boardSize; row++)
+                {
+                    if ((col + row) % 2 == 0)
+                    {
+                        button[col, row].BackgroundImage = checker;
+                        button[col, row].Tag = color;
+                        button[col, row].Text = button[col, row].Tag.ToString(); 
+                    }
+                }
+            }
+        }
+        private void InitializeBottom(Bitmap checker, string color)
+        {
+
+            int startingColumn = 5;
+
+            for (int col = startingColumn; col < BOARD_SIZE; col++)
+            {
+                for (int row = 0; row < BOARD_SIZE; row++)
+                {
+                    if ((col + row) % 2 == 0)
+                    {
+                        button[col, row].BackgroundImage = checker;
+                        button[col, row].Tag = color;
+                        button[col, row].Text = button[col, row].Tag.ToString();
+                    }
+                }
+            }
+        }
+    }
+}
